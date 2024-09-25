e0ce2391bf6fc1d94351d00e81aa84c510f7b47d
diegolopez-dev <diego.teide@gmail.com>
2024-09-25 12:52:22 +0200

    first commit

---
+++ holamundo-springboot/src/main/java/com/tcna/holamundo/HolaMundo.java
@@ -0,0 +1,18 @@
+package com.tcna.holamundo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/controller")
+public class HolaMundo {
+
+    @GetMapping("/holamundo")
+    @ResponseBody
+    public String mostrarMensaje() {
+        return "Hola mundo en Spring Boot";
+    }
+
+}
